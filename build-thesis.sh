2a6ec208601efd0549fa23ed8ad4e5d3ee69b073
Max-Julian Pogner <max-julian@pogner.at>
2021-09-03 18:03:03 +0200

    build-thesis: communicate user-provided value via CLI, support directory

--- build-thesis.sh
+++ build-thesis.sh
@@ -1,8 +1,41 @@
 #!/bin/sh
 # Copyright (C) 2014-2020 by Thomas Auzinger <thomas@auzinger.name>
 
-# Replace the 'x' in the next line with the name of the thesis' main LaTeX document without the '.tex' extension
-SOURCE=x
+usage() {
+    echo "Usage: $0 path/to/THESIS.tex" >&2
+}
+
+# parse command line argument for the "SOURCE" variable value
+SOURCE=
+WORKDIR=
+while [ $# -ge 1 ];
+do
+    arg="$1"
+    case "$arg" in
+        -h|--help)
+            usage
+            exit 1
+            ;;
+        *)
+            # the value of SOURCE is the given file name without the '.tex'
+            # extension.
+            # the value of WORKDIR is the directory where the filename
+            # resides.
+            SOURCE="$(basename "$arg" .tex)"
+            WORKDIR="$(dirname "$arg")"
+            ;;
+    esac
+    shift
+done
+
+[ -n "$SOURCE" ] || {
+    usage
+    exit 1
+}
+
+cd "$WORKDIR"
+
+echo "building thesis '$SOURCE' in directory '$WORKDIR'" >&2
 
 # Build the thesis document
 pdflatex "$SOURCE"
